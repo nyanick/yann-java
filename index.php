9c64dae8b7352fdbb5d2eef8137a01813567b46c
nde yanick <yanicknde@gmail.com>
2020-04-23 17:35:02 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->number[0];
+
+	if($text>=6){
+		$speech = "pleasen select a valid number.";
+	}
+	else{
+		$speech = "You selected ".$text;
+	}
+
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+
+?>
